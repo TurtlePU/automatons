c4c909d62fc8be3c4bf9de14a30426b195c834b8
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-05-14 10:54:23 +0300

    + basic workflow

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,22 @@
+import * as PIXI from 'pixi.js';
 
+const STATE_ZERO = 0;
+const STATE_READY = 1;
+
+var state = STATE_ZERO;
+
+window.onload = () => {
+    init_grid();
+    state = STATE_READY;
+}
+
+var app: PIXI.Application;
+
+function init_grid() {
+    app = new PIXI.Application({
+        width: 800,
+        height: 600,
+        backgroundColor: 0
+    });
+    document.body.appendChild(app.view);
+}
